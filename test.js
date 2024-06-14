30a619e8122089d9027109c2b36a0833725fca31
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-14 11:54:25 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-axios.get('http://localhost:3000/contacts')
+// axios.get('http://localhost:3000/contacts')
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+
+axios.get('http://localhost:3000/contacts/666bf52764b73a53690bbd52')
     .then(response => {
         console.log(response.data);
     })
