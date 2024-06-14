782f078186b2eb82cbb104abbaf2d95c470c0d77
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-14 10:19:21 +0300

    contactSchema

---
+++ src/db/models/contact.js
@@ -0,0 +1,36 @@
+// src/db/models/contact.js
+
+import {
+    model,
+    Schema
+} from 'mongoose';
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    isFavourite: {
+        type: Boolean,
+        default: false,
+    },
+    contactType: {
+        type: String,
+        enum: ['work', 'home', 'personal'],
+        required: true,
+        default: 'personal',
+    },
+}, {
+    timestamps: true,
+    versionKey: false,
+});
+
+export const Contact = model('Contact', contactSchema);
