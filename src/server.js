8018b8168420db7b3be8c7f56e989fdf43c421b3
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-24 20:09:03 +0300

    2nd commit, src/server.js setup

--- src/server.js
+++ src/server.js
@@ -4,53 +4,55 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {
-    getContacts,
-    getContact
-} from './controllers/contactsController.js';
+import { getContacts, getContact } from './controllers/contactsController.js';
+import router from './routers/contacts.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 export const setupServer = () => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
-    app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
-    app.use(pino({
-        transport: {
-            target: 'pino-pretty',
-        },
-    }));
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.get('/', (req, res) => {
-        res.json({
-            message: 'Hello world! This index page belongs to hw2-mongodb branch',
-        });
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world! This index page belongs to hw3-crud branch',
     });
+  });
 
-    // Реєстрація роута GET /contacts
-    app.get('/contacts', getContacts);
+  app.use(router); // Додаємо роутер до app як middleware
 
-    // Реєстрація роута GET /contacts/:contactId
-    app.get('/contacts/:contactId', getContact);
+  // Реєстрація роута GET /contacts
+  app.get('/contacts', getContacts);
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
+  // Реєстрація роута GET /contacts/:contactId
+  app.get('/contacts/:contactId', getContact);
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
     });
+  });
 
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
     });
+  });
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
