561cbdecbcaf7362045b6d2fca628718cea814de
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-24 20:53:53 +0300

    step 1 WiP

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,32 +1,11 @@
 // src/routers/contacts.js
 
 import { Router } from 'express';
-import { getAllContacts, getContactById } from '../services/contacts';
+import { getContacts, getContact } from '../controllers/contactsController';
 
 const router = Router();
 
-router.get(`/contacts`, async (req, res) => {
-  const contacts = await getAllContacts();
-
-  res.status(200).json({
-    data: contacts,
-  });
-});
-
-app.get(`/contacts/:contactId`, async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-
-  if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
-    return;
-  }
-
-  res.status(200).json({
-    data: contact,
-  });
-});
+router.get('/contacts', getContacts);
+router.get('/contacts/:contactId', getContact);
 
 export default router;
