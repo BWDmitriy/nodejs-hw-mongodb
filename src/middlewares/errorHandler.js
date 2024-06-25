1452223e9be41bda6d11de9d9dbb3961d6aae5b5
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-25 11:11:58 +0300

    errorHandler updated

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -8,14 +8,15 @@ export const errorHandler = (err, req, res, next) => {
     if (err instanceof HttpError) {
         res.status(err.status).json({
             status: err.status,
-            message: err.name,
-            data: err,
+            message: 'Something went wrong',
+            data: err.message,
         });
         return;
     }
 
     res.status(500).json({
+        status: 500,
         message: 'Something went wrong',
-        error: err.message,
+        data: err.message,
     });
 };
--- src/server.js
+++ src/server.js
@@ -41,7 +41,6 @@ export const setupServer = () => {
     });
   });
 
-  // Use the contacts router
   app.use('/api', router);
 
   app.use('*', notFoundHandler);
