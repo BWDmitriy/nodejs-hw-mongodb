70482d7b61a4996cb72eff31faeb9f28da64521b
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-25 22:59:41 +0300

    Step 4 confirmed

--- test.js
+++ test.js
@@ -5,18 +5,37 @@ import axios from "axios";
 // Get all contacts
 // node test.js
 
-axios.get('http://localhost:3000/contacts')
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+// axios.get('http://localhost:3000/api/contacts')
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
 
 //Get contact by sample id
 //node test.js
 
-// axios.get('http://localhost:3000/contacts/666bf52764b73a53690bbd52')
+// axios.get('http://localhost:3000/api/contacts/666bf52764b73a53690bbd52')
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+
+// Add (post) contact with { data }
+//node test.js
+
+// const data = {
+//     name: "John Doe",
+//     phoneNumber: "1234567890",
+//     email: "john.doe@example.com",
+//     isFavourite: true,
+//     contactType: "work"
+// };
+
+// axios.post('http://localhost:3000/api/contacts', data)
 //     .then(response => {
 //         console.log(response.data);
 //     })
