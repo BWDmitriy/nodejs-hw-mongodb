e3333afc02dc5654a0b6daade30d657485e32110
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-25 22:13:42 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -12,7 +12,7 @@ export const getContactById = async (id) => {
     return await ContactsCollection.findById(id);
 };
 
-export const addContact = async (contactData) => {
-    const newContact = new ContactsCollection(contactData);
-    return await newContact.save();
+export const addContact = async (payload) => {
+    const newContact = await ContactsCollection.create(payload);
+    return newContact;
 };
