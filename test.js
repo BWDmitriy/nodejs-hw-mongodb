50d2603929753a71a5017e9530316f1d7f2446e4
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-25 22:52:22 +0300

    testing

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-//node src/index.js
+// node src/index.js
 
-//Get all contacts
-//node test.js
+// Get all contacts
+// node test.js
 
-// axios.get('http://localhost:3000/contacts')
-//     .then(response => {
-//         console.log(response.data);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
+axios.get('http://localhost:3000/contacts')
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 //Get contact by sample id
 //node test.js
