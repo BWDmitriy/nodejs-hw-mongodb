a39860857473d378716cefe689f7a0ab62d2c9a2
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-28 16:07:51 +0300

    validation fixes, routing upd

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,25 +1,34 @@
 // src/routers/contacts.js
 
+import { Router } from 'express';
 import {
-    Router
-} from 'express';
-import {
-    getContacts,
-    getContact,
-    createContact,
-    updateContact,
-    deleteContact
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
 } from '../controllers/contactsController.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validation.js';
 import {
-    ctrlWrapper
-} from '../utils/ctrlWrapper.js';
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContacts));
 router.get('/contacts/:contactId', ctrlWrapper(getContact));
-router.post('/contacts', ctrlWrapper(createContact));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContact));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContact),
+);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContact),
+);
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContact));
 
 export default router;
