4c6cb01e9398affa7e3e746f3664598b095e5baa
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-06-28 16:36:04 +0300

    Step 3 WiP, services/contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,28 +1,27 @@
 // src/services/contacts.js
 
-import {
-    ContactsCollection
-} from '../db/models/contact.js';
+import { ContactsCollection } from '../db/models/contact.js';
 
-export const getAllContacts = async () => {
-    return await ContactsCollection.find();
+export const getAllContacts = async ({ page = 1, perPage = 10 }) => {
+  const skip = (page - 1) * perPage;
+  return await ContactsCollection.find().skip(skip).limit(perPage);
 };
 
 export const getContactById = async (id) => {
-    return await ContactsCollection.findById(id);
+  return await ContactsCollection.findById(id);
 };
 
 export const addContact = async (payload) => {
-    const newContact = await ContactsCollection.create(payload);
-    return newContact;
+  const newContact = await ContactsCollection.create(payload);
+  return newContact;
 };
 
 export const patchContact = async (id, contactData) => {
-    return await ContactsCollection.findByIdAndUpdate(id, contactData, {
-        new: true
-    });
+  return await ContactsCollection.findByIdAndUpdate(id, contactData, {
+    new: true,
+  });
 };
 
 export const removeContact = async (id) => {
-    return await ContactsCollection.findByIdAndDelete(id);
+  return await ContactsCollection.findByIdAndDelete(id);
 };
