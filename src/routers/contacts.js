f61e20e2d14579bddd791d257abacec8ea1a9239
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-07-01 20:24:22 +0300

    Step 2 confirmed

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,34 +1,34 @@
 // src/routers/contacts.js
 
 import { Router } from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   getContacts,
   getContact,
   createContact,
   updateContact,
   deleteContact,
-} from '../controllers/contactsController';
-import { validateBody } from '../middlewares/validation';
-import { contactSchema } from '../validation/contact';
-import { authenticate } from '../middlewares/authenticate';
+} from '../controllers/contactsController.js';
+import { validateBody } from '../middlewares/validation.js';
+import { contactSchema } from '../validation/contact.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', authenticate, ctrlWrapper(getContacts));
-contactsRouter.get('/:contactId', authenticate, ctrlWrapper(getContact));
+contactsRouter.use(authenticate);
+
+contactsRouter.get('/', ctrlWrapper(getContacts));
+contactsRouter.get('/:contactId', ctrlWrapper(getContact));
 contactsRouter.post(
   '/',
-  authenticate,
   validateBody(contactSchema),
   ctrlWrapper(createContact),
 );
 contactsRouter.patch(
   '/:contactId',
-  authenticate,
   validateBody(contactSchema),
   ctrlWrapper(updateContact),
 );
-contactsRouter.delete('/:contactId', authenticate, ctrlWrapper(deleteContact));
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteContact));
 
 export default contactsRouter;
