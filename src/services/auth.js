75e2fdfd34a5730f5df6bcdc41507943ee0d3179
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-07-01 14:50:59 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,9 +6,7 @@ import { UsersCollection } from '../db/models/user.js';
 import createHttpError from 'http-errors';
 
 export const registerUser = async (payload) => {
-  const user = await UsersCollection.findOne({
-    email: payload.email,
-  });
+  const user = await UsersCollection.findOne({ email: payload.email });
   if (user) throw createHttpError(409, 'Email in use');
 
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
@@ -17,4 +15,18 @@ export const registerUser = async (payload) => {
     ...payload,
     password: encryptedPassword,
   });
+};
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({
+    email: payload.email,
+  });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
 };
