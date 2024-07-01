a98c0cfa19a881156fb6b80d38ff5e737848e109
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-07-01 21:45:40 +0300

    Step 5-7 confirmed

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,48 +1,38 @@
 // src/middlewares/authenticate.js
 
 import createHttpError from 'http-errors';
-
 import { SessionsCollection } from '../db/models/session.js';
 import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  try {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    next(createHttpError(401, 'Please provide Authorization header'));
-    return;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw createHttpError(401, 'Authorization header missing or malformed');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const session = await SessionsCollection.findOne({ accessToken: token });
+
+    if (!session) {
+      throw createHttpError(401, 'Invalid access token');
+    }
+
+    if (new Date() > new Date(session.accessTokenValidUntil)) {
+      throw createHttpError(401, 'Access token expired');
+    }
+
+    const user = await UsersCollection.findById(session.userId);
+
+    if (!user) {
+      throw createHttpError(401, 'User not found');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
-
-  if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer'));
-    return;
-  }
-
-  const session = await SessionsCollection.findOne({ accessToken: token });
-
-  if (!session) {
-    next(createHttpError(401, 'Session not found'));
-    return;
-  }
-
-  const isAccessTokenExpired =
-    new Date() > new Date(session.accessTokenValidUntil);
-
-  if (isAccessTokenExpired) {
-    next(createHttpError(401, 'Access token expired'));
-  }
-
-  const user = await UsersCollection.findById(session.userId);
-
-  if (!user) {
-    next(createHttpError(401));
-    return;
-  }
-
-  req.user = user;
-
-  next();
 };
