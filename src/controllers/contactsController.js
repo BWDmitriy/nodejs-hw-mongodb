3e1d800630835cc2fb0d328da339f0909a4c9d44
Dmytro Zl <82403947+BWDmitriy@users.noreply.github.com>
2024-07-04 10:18:59 +0300

    Update contactsController.js

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,43 +1,38 @@
 // src/controllers/contactsController.js
 
-import {
-    getAllContacts,
-    getContactById
-} from '../services/contacts.js';
+import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContacts = async (req, res, next) => {
-    try {
-        const contacts = await getAllContacts();
-        res.json({
-            status: "success",
-            message: "Successfully found contacts!",
-            data: contacts,
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const contacts = await getAllContacts();
+    res.json(200)({
+      status: '200',
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getContact = async (req, res, next) => {
-    try {
-        const {
-            contactId
-        } = req.params;
-        const contact = await getContactById(contactId);
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
 
-        if (!contact) {
-            return res.status(404).json({
-                status: "error",
-                message: `Contact with id ${contactId} not found`,
-            });
-        }
-
-        res.json({
-            status: "success",
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    } catch (error) {
-        next(error);
+    if (!contact) {
+      return res.status(404).json({
+        status: '404',
+        message: `Contact with id ${contactId} not found`,
+      });
     }
+
+    res.json(200)({
+      status: '200',
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
