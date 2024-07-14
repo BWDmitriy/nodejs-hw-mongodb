8cb38112795869d9271e55988c85eb26e4f6851d
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-07-14 15:13:11 +0300

    controllers refactored

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -42,66 +42,56 @@ export const loginUserController = async (req, res) => {
 };
 
 export const logoutUserController = async (req, res, next) => {
-  try {
-    console.log('Cookies:', req.cookies);
-    const sessionId = req.cookies.sessionId;
-    console.log('Session ID:', sessionId);
+  console.log('Cookies:', req.cookies);
+  const sessionId = req.cookies.sessionId;
+  console.log('Session ID:', sessionId);
 
-    if (!sessionId || !isValidObjectId(sessionId)) {
-      return next(createHttpError(400, 'Invalid session ID'));
-    }
-
-    const sessionExists = await logoutUser(sessionId);
-
-    if (!sessionExists) {
-      return next(createHttpError(404, 'Session not found'));
-    }
-
-    res.clearCookie('sessionId');
-    res.clearCookie('refreshToken');
-
-    res.status(204).send();
-  } catch (error) {
-    console.error('Logout error:', error);
-    next(error);
+  if (!sessionId || !isValidObjectId(sessionId)) {
+    return next(createHttpError(400, 'Invalid session ID'));
   }
+
+  const sessionExists = await logoutUser(sessionId);
+
+  if (!sessionExists) {
+    return next(createHttpError(404, 'Session not found'));
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('refreshToken');
+
+  res.sendStatus(204);
 };
 
 export const refreshUserSessionController = async (req, res, next) => {
-  try {
-    console.log('Cookies:', req.cookies);
-    const sessionId = req.cookies.sessionId;
-    const refreshToken = req.cookies.refreshToken;
-    console.log('Session ID:', sessionId);
-    console.log('Refresh Token:', refreshToken);
+  console.log('Cookies:', req.cookies);
+  const sessionId = req.cookies.sessionId;
+  const refreshToken = req.cookies.refreshToken;
+  console.log('Session ID:', sessionId);
+  console.log('Refresh Token:', refreshToken);
 
-    if (!sessionId || !refreshToken) {
-      return next(createHttpError(400, 'Missing session ID or refresh token'));
-    }
-
-    const session = await refreshUsersSession({
-      sessionId,
-      refreshToken,
-    });
-
-    res.cookie('refreshToken', session.refreshToken, {
-      httpOnly: true,
-      expires: new Date(Date.now() + ONE_DAY * 30),
-    });
-    res.cookie('sessionId', session._id, {
-      httpOnly: true,
-      expires: new Date(Date.now() + ONE_DAY * 30),
-    });
-
-    res.json({
-      status: 200,
-      message: 'Successfully refreshed a session!',
-      data: {
-        accessToken: session.accessToken,
-      },
-    });
-  } catch (error) {
-    console.error('Refresh session error:', error);
-    next(error);
+  if (!sessionId || !refreshToken) {
+    return next(createHttpError(400, 'Missing session ID or refresh token'));
   }
+
+  const session = await refreshUsersSession({
+    sessionId,
+    refreshToken,
+  });
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY * 30),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY * 30),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -11,175 +11,155 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export const getContacts = async (req, res, next) => {
-  try {
-    const {
-      page = 1,
-      perPage = 10,
-      sortBy = 'name',
-      sortOrder = 'asc',
-      type,
-      isFavourite,
-    } = req.query;
-    const { contacts, totalItems } = await getAllContacts({
-      page,
-      perPage,
-      sortBy,
-      sortOrder,
-      type,
-      isFavourite,
-      userId: req.user._id,
-    });
-    const totalPages = Math.ceil(totalItems / perPage);
+  const {
+    page = 1,
+    perPage = 10,
+    sortBy = 'name',
+    sortOrder = 'asc',
+    type,
+    isFavourite,
+  } = req.query;
+  const { contacts, totalItems } = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    type,
+    isFavourite,
+    userId: req.user._id,
+  });
+  const totalPages = Math.ceil(totalItems / perPage);
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: {
-        data: contacts,
-        page: parseInt(page),
-        perPage: parseInt(perPage),
-        totalItems,
-        totalPages,
-        hasPreviousPage: page > 1,
-        hasNextPage: page < totalPages,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: {
+      data: contacts,
+      page: parseInt(page),
+      perPage: parseInt(perPage),
+      totalItems,
+      totalPages,
+      hasPreviousPage: page > 1,
+      hasNextPage: page < totalPages,
+    },
+  });
 };
 
 export const getContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    if (!isValidObjectId(contactId)) {
-      return next(
-        createHttpError(400, {
-          status: 400,
+  if (!isValidObjectId(contactId)) {
+    return next(
+      createHttpError(400, {
+        status: 400,
+        message: 'Invalid contact ID',
+        data: {
           message: 'Invalid contact ID',
-          data: {
-            message: 'Invalid contact ID',
-          },
-        }),
-      );
-    }
-
-    const contact = await getContactById(contactId, req.user._id);
-
-    if (!contact) {
-      return next(
-        createHttpError(404, {
-          status: 404,
-          message: 'Contact not found',
-          data: {
-            message: 'Contact not found',
-          },
-        }),
-      );
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  } catch (error) {
-    next(error);
+        },
+      }),
+    );
   }
+
+  const contact = await getContactById(contactId, req.user._id);
+
+  if (!contact) {
+    return next(
+      createHttpError(404, {
+        status: 404,
+        message: 'Contact not found',
+        data: {
+          message: 'Contact not found',
+        },
+      }),
+    );
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
 };
 
 export const createContact = async (req, res, next) => {
-  try {
-    const contactData = { ...req.body, userId: req.user._id };
-    const newContact = await addContact(contactData);
+  const contactData = { ...req.body, userId: req.user._id };
+  const newContact = await addContact(contactData);
 
-    res.status(201).json({
-      status: 201,
-      message: 'Successfully created a contact!',
-      data: newContact,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: newContact,
+  });
 };
 
 export const updateContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    if (!isValidObjectId(contactId)) {
-      return next(
-        createHttpError(400, {
-          status: 400,
+  if (!isValidObjectId(contactId)) {
+    return next(
+      createHttpError(400, {
+        status: 400,
+        message: 'Invalid contact ID',
+        data: {
           message: 'Invalid contact ID',
-          data: {
-            message: 'Invalid contact ID',
-          },
-        }),
-      );
-    }
-
-    const contactData = req.body;
-    const updatedContact = await patchContact(
-      contactId,
-      contactData,
-      req.user._id,
+        },
+      }),
     );
-
-    if (!updatedContact) {
-      return next(
-        createHttpError(404, {
-          status: 404,
-          message: 'Contact not found',
-          data: {
-            message: 'Contact not found',
-          },
-        }),
-      );
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully patched a contact!',
-      data: updatedContact,
-    });
-  } catch (error) {
-    next(error);
   }
+
+  const contactData = req.body;
+  const updatedContact = await patchContact(
+    contactId,
+    contactData,
+    req.user._id,
+  );
+
+  if (!updatedContact) {
+    return next(
+      createHttpError(404, {
+        status: 404,
+        message: 'Contact not found',
+        data: {
+          message: 'Contact not found',
+        },
+      }),
+    );
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: updatedContact,
+  });
 };
 
 export const deleteContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    if (!isValidObjectId(contactId)) {
-      return next(
-        createHttpError(400, {
-          status: 400,
+  if (!isValidObjectId(contactId)) {
+    return next(
+      createHttpError(400, {
+        status: 400,
+        message: 'Invalid contact ID',
+        data: {
           message: 'Invalid contact ID',
-          data: {
-            message: 'Invalid contact ID',
-          },
-        }),
-      );
-    }
-
-    const deletedContact = await removeContact(contactId, req.user._id);
-    if (!deletedContact) {
-      return next(
-        createHttpError(404, {
-          status: 404,
-          message: 'Contact not found',
-          data: {
-            message: 'Contact not found',
-          },
-        }),
-      );
-    }
-
-    res.status(204).send();
-  } catch (error) {
-    next(error);
+        },
+      }),
+    );
   }
+
+  const deletedContact = await removeContact(contactId, req.user._id);
+  if (!deletedContact) {
+    return next(
+      createHttpError(404, {
+        status: 404,
+        message: 'Contact not found',
+        data: {
+          message: 'Contact not found',
+        },
+      }),
+    );
+  }
+
+  res.sendStatus(204);
 };
