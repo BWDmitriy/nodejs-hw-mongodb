a586ab111e6c0a92315b9502bb2cf5dca85ff434
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-07-15 11:22:41 +0300

    sendMail fixes

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,17 +2,27 @@
 
 import nodemailer from 'nodemailer';
 
-import { env } from '../utils/env.js';
+// import { env } from '../utils/env.js';
+
+import { SMTP } from '../constants/index.js';
 
 const transporter = nodemailer.createTransport({
-  host: env('SMTP_HOST'),
-  port: Number(env('SMTP_PORT')),
+  host: SMTP.SMTP_HOST,
+  port: SMTP.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
   auth: {
-    user: env('SMTP_USER'),
-    pass: env('SMTP_PASSWORD'),
+    user: SMTP.SMTP_USER,
+    pass: SMTP.SMTP_PASSWORD,
   },
 });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+export const sendEmail = async ({ from, to, subject, html }) => {
+  const info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    html,
+  });
+
+  console.log('Message sent: %s', info.messageId);
 };
